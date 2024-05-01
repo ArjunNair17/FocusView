a15a2f9ed451f826c14ce63f6bc24f2a0745f27d
Adarsh Chilkunda <adroyalz@gmail.com>
2024-05-01 11:19:02 -0700

    move firebase setup into a config folder

--- server/index.js
+++ server/index.js
@@ -3,32 +3,11 @@ require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const app = express();
 const testRoutes = require("./routes/api/tests");
-
-const { initializeApp } = require("firebase/app");
-const {
-  getFirestore,
-  collection,
-  getDocs,
-  listCollections,
-} = require("firebase/firestore");
+const db = require("./config/firebase");
+const { collection, getDocs } = require("firebase/firestore");
 
 const hostname = "127.0.0.1";
 const port = 8080;
-
-//FIRESTORE
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
-
-// Initialize Firebase
-const fire = initializeApp(firebaseConfig);
-const db = getFirestore(fire);
 
 //DEFAULT get route
 app.get("/", (req, res) => {

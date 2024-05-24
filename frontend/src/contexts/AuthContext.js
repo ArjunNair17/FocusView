0d4f1653831231a8627b078e60031b9eaaec233e
Helen Feng <53279829+hailenwashere@users.noreply.github.com>
2024-05-23 18:32:31 -0700

    add context for error msgs

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,19 @@
+// AuthContext.js
+
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const value = {};
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+}

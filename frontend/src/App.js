ce04c49af82d499824574f9527090c106a1daa73
sanjanarathore <sanjanar232@gmail.com>
2024-05-29 19:40:47 -0700

    added sign in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import Button from '@mui/material/Button';
 import Page1 from './page_1'; 
 import Page2 from './page_2'; 
 import { Page3 } from './page_3';
+import  SignIn from './signin';
 import Stack from '@mui/material/Stack';
 
 
@@ -24,6 +25,7 @@ function App() {
         <Route path="/page_1" element={<Page1 />} />
         <Route path="/page_2" element={<Page2 />} />
         <Route path="/page_3" element={<Page3 />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   );
